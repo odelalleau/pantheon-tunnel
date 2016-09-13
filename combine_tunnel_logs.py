f5a67d7d2e4be52077498ab81b0d663f8962084a
Greg D. Hill <greghill@cs.stanford.edu>
2016-09-13 15:45:06 -0700

    rename some vars in combine_tunnel_logs.py

--- combine_tunnel_logs.py
+++ combine_tunnel_logs.py
@@ -1,32 +1,32 @@
 #!/usr/bin/python
-egress_packets = dict()
+client_packets = dict()
 with open('/tmp/tunnelclient.egress.log') as client_egress_log:
     firstline = True
     for line in client_egress_log:
         if firstline:
-            ( _, _, egress_initial_timestamp ) = line.split(':')
+            ( _, _, client_initial_timestamp ) = line.split(':')
             firstline = False
         else:
             (timestamp, uid, size) = line.split('-')
-            egress_packets[uid] = (timestamp, size)
+            client_packets[uid] = (timestamp, size)
 
 unsorted_log = []
 with open('/tmp/tunnelserver.ingress.log') as server_ingress_log:
     firstline = True
     for line in server_ingress_log:
         if firstline:
-            ( _, _, ingress_initial_timestamp ) = line.split(':')
+            ( _, _, server_initial_timestamp ) = line.split(':')
             firstline = False
         else:
             (timestamp, uid, size) = line.split('-')
-            ingress_timestamp = int(timestamp) - (int(egress_initial_timestamp) - int(ingress_initial_timestamp))
-            if uid in egress_packets:
-                (egress_timestamp, egress_size) = egress_packets[uid]
-                if size != egress_size:
-                    print("packet " + uid + " came into tunnel with size " + egress_size + " but left with size " + size)
+            server_timestamp = int(timestamp) - (int(client_initial_timestamp) - int(server_initial_timestamp))
+            if uid in client_packets:
+                (client_timestamp, client_size) = client_packets[uid]
+                if size != client_size:
+                    print("packet " + uid + " came into tunnel with size " + client_size + " but left with size " + size)
                     assert( False )
-                unsorted_log.append( egress_timestamp.strip() + ' + ' + str(int(size)) )
-                unsorted_log.append( str(ingress_timestamp) + ' - ' + str(int(size)) + ' ' + str( ingress_timestamp - int(egress_timestamp) ) )
+                unsorted_log.append( str(int(client_timestamp)) + ' + ' + str(int(size)) )
+                unsorted_log.append( str(int(server_timestamp)) + ' - ' + str(int(size)) + ' ' + str( server_timestamp - int(client_timestamp) ) )
 
 print("# base timestamp: 0" )
 for line in sorted( unsorted_log ):
