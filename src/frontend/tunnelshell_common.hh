9f3a177c845a91209889e181d055a1d45c1fcc84
Greg D. Hill <greghill@cs.stanford.edu>
2016-10-31 19:16:47 -0700

    tunnelshells using --interface also require /proc/sys/net/ipv4/conf/all/rp_filter=0

--- src/frontend/tunnelshell_common.hh
+++ src/frontend/tunnelshell_common.hh
@@ -3,13 +3,17 @@
 
 void check_interface_for_binding( const std::string &prog_name, const std::string &if_name ) {
     /* verify interface exists and has rp filtering disabled before binding to an interface */
-    std::string rp_filter_path = "/proc/sys/net/ipv4/conf/" + if_name + "/rp_filter";
+    const std::string interface_rpf_path = "/proc/sys/net/ipv4/conf/" + if_name + "/rp_filter";
+    const std::string all_rpf_path = "/proc/sys/net/ipv4/conf/all/rp_filter";
 
-    if ( access( rp_filter_path.c_str(), R_OK ) == -1 ) {
-        throw runtime_error( prog_name + ": can't read " + rp_filter_path + ", interface \"" + if_name + "\" probably doesn't exist" );
+    if ( access( interface_rpf_path.c_str(), R_OK ) == -1 ) {
+        throw runtime_error( prog_name + ": can't read " + interface_rpf_path + ", interface \"" + if_name + "\" probably doesn't exist" );
     }
-    FileDescriptor rpf( SystemCall( "open " + rp_filter_path, open( rp_filter_path.c_str(), O_RDONLY ) ) );
-    if ( rpf.read() != "0\n" ) {
-        throw runtime_error( prog_name + ": Can only run if \"cat " + rp_filter_path + "\" = 0" );
+    FileDescriptor rpf_interface( SystemCall( "open " + interface_rpf_path, open( interface_rpf_path.c_str(), O_RDONLY ) ) );
+    FileDescriptor rpf_all( SystemCall( "open " + all_rpf_path, open( all_rpf_path.c_str(), O_RDONLY ) ) );
+
+    if ( rpf_interface.read() != "0\n" or rpf_all.read() != "0\n" ) {
+        throw runtime_error( prog_name + ": Can only run using --interface if both \"cat " + all_rpf_path
+                + "\"= 0 and \"cat " + interface_rpf_path  + "\"= 0" );
     }
 }
